28ea4335468d103a742383a7a9be3fcfed7deb06
Micaela Rodas <micarodas16@gmail.com>
2023-01-05 03:00:51 -0300

    arreglé errores

--- simulador_uno.js
+++ simulador_uno.js
@@ -1,15 +1,28 @@
 alert("Bienvenida/o a Bakugo Store");
 
+class Producto {
+    constructor(nombre, talle, precio, stock){
+    this.nombre = nombre;
+    this.talle = talle;
+    this.precio = precio;
+    this.stock = stock;
+    }
+}
+
+let remera = new Producto ("remera_a", "M" , 2000, 3 );
+let pantalon = new Producto ("pantalon_a", "M" , 2500, 3 );
+let buzo = new Producto ("buzo_a", "M" , 3000, 3 );
+let zapatillas = new Producto ("zapatillas_a", "M" , 5000, 3 );
 
 const precioRemera = 2000;
 const precioPantalon = 2500;
 const precioBuzo = 3000;
 const precioZapatillas = 5000;
 
-let remeras = parseInt(prompt("cuantas remeras va a comprar?"));
-let pantalones = parseInt(prompt("cuantas pantalones va a comprar?"));
-let buzos = parseInt(prompt("cuantos buzos va a comprar?"));
-let zapatillas = parseInt(prompt("cuantas zapatillas va a comprar?"));
+let remerasCantidad = parseInt(prompt("cuantas remeras va a comprar?"));
+let pantalonesCantidad = parseInt(prompt("cuantas pantalones va a comprar?"));
+let buzosCantidad = parseInt(prompt("cuantos buzos va a comprar?"));
+let zapatillasCantidad = parseInt(prompt("cuantas zapatillas va a comprar?"));
 
 function costoTotal (remeras,pantalones,buzos,zapatillas){
     
@@ -26,4 +39,4 @@ function costoTotal (remeras,pantalones,buzos,zapatillas){
     }
 }
 
-costoTotal(remeras,pantalones,buzos,zapatillas)
+costoTotal(remera, pantalon,buzo,zapatillas)
